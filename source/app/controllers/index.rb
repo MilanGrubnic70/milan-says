ecae1f4c1d3372cf51271453a290ae9e578c467c
Milan Grubnic <MilanG@aol.com>
2014-06-20 15:17:17 -0500

    initial files

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -7,10 +7,11 @@ post '/color' do
 
   #Create and return a JSON object with the random cell and color given below.
 
-  cell= rand(1..9)
-  color= "#" + "%06x" % (rand * 0xffffff)
+  cell= rand(1..12)
+  # color= "#" + "%06x" % (rand * 0xffffff)
+  icon = rand(1..17)
 
-  {cell: cell, color: color}.to_json
+  {cell: cell, icon: icon}.to_json
 end
 
 #########
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,9 +1,35 @@
+
+// $(document).ready(function(){
+//     $('#get_cohort').click(function(event){
+//         alert("HI")
+//       event.preventDefault();
+//         $.post('/color', function(response){
+//           $("#color_me li:nth-child(" + response.cell +")").replaceWith( "<li>ff" + response.cohort + "</li>" );
+//         }, 'json');
+//     });
+// });
+
 $(document).ready(function(){
-    $('#get_color').click(function(event){
-      event.preventDefault();
-        $.post('/color', function(response){
-          $("#color_me li:nth-child(" + response.cell +")").css( "background-color", response.color );
-        }, 'json');
-    });
+    $('img').click(function(){
+        $(this).effect('explode');
+    });    
 });
 
+// var cohorts=[ff1,ff2,ff3,ff4,ff5,ff6,ff7,ff8,ff9,ff10,ff11,ff12,ff13,ff14,ff15,ff16,ff17]
+// var ff1 = <img src="https://secure.gravatar.com/avatar/b235791358a6dbb3a5ed7a065fb79da5.png?r=PG&amp;d=mm&amp;s=98" title="Ahmed Al-Bahar">
+// var ff2 = <img src="https://secure.gravatar.com/avatar/0ac706d64f461dfa204e7f5201e9a0b7.png?r=PG&amp;d=mm&amp;s=98" title="Alexander Glassman">
+// var ff3 = <img src="https://secure.gravatar.com/avatar/5faf5aa1d7d51f6d7152fecf1f7c896c.png?r=PG&amp;d=mm&amp;s=98" title="Caleb Rugg">
+// var ff4 = <img src="https://secure.gravatar.com/avatar/fcdebb589fc2f11e58b4666ed32abba6.png?r=PG&amp;d=mm&amp;s=98" title="Catherine Vongsathorn">
+// var ff5 = <img src="https://secure.gravatar.com/avatar/490d045d97c6223541bb62a36edf0314.png?r=PG&amp;d=mm&amp;s=98" title="Daniel Yoachim">
+// var ff6 = <img src="https://secure.gravatar.com/avatar/cdee5e2e3c95a0537d981e1f8d1d21ad.png?r=PG&amp;d=mm&amp;s=98" title="Elizabeth Kaplan">
+// var ff7 = <img src="https://secure.gravatar.com/avatar/4d2d447e555f5c0c19f1fbc48a434cc0.png?r=PG&amp;d=mm&amp;s=98" title="Keenan Turner">
+// var ff8 = <img src="https://secure.gravatar.com/avatar/f89a944077bac5545411b45a2159d262.png?r=PG&amp;d=mm&amp;s=98" title="Kim Girard">
+// var ff9 = <img src="https://secure.gravatar.com/avatar/3ba35b7236a98c7bf2f856cf8403a72a.png?r=PG&amp;d=mm&amp;s=98" title="Michael Pourhadi">
+// var ff10 = <img src="https://secure.gravatar.com/avatar/5c60be2dc54d0a811ae1ce29e5c1d18b.png?r=PG&amp;d=mm&amp;s=98" title="Milan Grubnic">
+// var ff11 = <img src="https://secure.gravatar.com/avatar/494b8777cb25f8b0ccee451a5d3ca188.png?r=PG&amp;d=mm&amp;s=98" title="Monica Cho">
+// var ff12 = <img src="https://secure.gravatar.com/avatar/7d813c9ab3e89b048c3957048af01350.png?r=PG&amp;d=mm&amp;s=98" title="Natalie Frecka">
+// var ff13 = <img src="https://secure.gravatar.com/avatar/a1794745acd9e572c24558b78b48d597.png?r=PG&amp;d=mm&amp;s=98" title="Peter Debelak">
+// var ff14 = <img src="https://secure.gravatar.com/avatar/74e1c1044ac91b119233e5e78aa1e371.png?r=PG&amp;d=mm&amp;s=98" title="Richard Dsida">
+// var ff15 = <img src="https://secure.gravatar.com/avatar/5f22cb686424df7247ee54422b6412f1.png?r=PG&amp;d=mm&amp;s=98" title="Scott Silveus">
+// var ff16 = <img src="https://secure.gravatar.com/avatar/ade32779223cdb91e657cffed5dd546f.png?r=PG&amp;d=mm&amp;s=98" title="Tom Hopkins">
+// var ff17 = <img src="https://secure.gravatar.com/avatar/d7e357500ce356bdfa58056791a5ae01.png?r=PG&amp;d=mm&amp;s=98" title="Zac Mitton">
